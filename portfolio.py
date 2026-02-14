6cfc3fab484aac972f8d5ba8a0b3a7ff20d8d305
mr3022 <leemiri3022@gmail.com>
2026-02-14 19:47:45 +0900

    Add files via upload

--- portfolio.py
+++ portfolio.py
@@ -1211,11 +1211,11 @@ if selected_tab == "성과":
                         font-size: 12px; font-weight: 600; color: #6c757d; margin-bottom: 8px;">
                 <div>Month</div>
                 <div style="text-align: right; margin-rihgt: 2px;">US Market</div>
-                <div style="text-align: right;">US AI</div>
+                <div style="text-align: right; margin-rihgt: 2px;">US AI</div>
                 <div style="text-align: right;">US WRAP</div>
                 <div style="text-align: right;">KR Leverage</div>
                 <div style="text-align: right;">KR ETF</div>
-                <div style="text-align: right;">Total</div>
+                <div style="text-align: right; margin-rihgt: 2px;">Total</div>
             </div>
             """
             
